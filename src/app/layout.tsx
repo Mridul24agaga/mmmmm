cb9456051975fdc11962ea3880ec2b344c74f8a1
Mridul24agaga <mridulthareja8@gmail.com>
2024-11-20 21:46:15 +0530

    HASHSAH

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import { Toaster } from "@/components/ui/toaster"
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin"
-import type { Metadata } from "next"
-import { ThemeProvider } from "next-themes"
-import localFont from "next/font/local"
-import { extractRouterConfig } from "uploadthing/server"
-import { fileRouter } from "./api/uploadthing/core"
-import "./globals.css"
-import ReactQueryProvider from "./ReactQueryProvider"
-import AdScript from "@/components/AdScript"
+import { Toaster } from "@/components/ui/toaster";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import localFont from "next/font/local";
+import { extractRouterConfig } from "uploadthing/server";
+import { fileRouter } from "./api/uploadthing/core";
+import "./globals.css";
+import ReactQueryProvider from "./ReactQueryProvider";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-})
-
+});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-})
+});
 
 export const metadata: Metadata = {
   title: {
@@ -25,7 +24,7 @@ export const metadata: Metadata = {
     default: "Memories Lived",
   },
   description: "The app for your loved ones!",
-}
+};
 
 export default function RootLayout({
   children,
@@ -34,6 +33,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7867911056689513"
+          crossOrigin="anonymous"
+          strategy="lazyOnload"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(fileRouter)} />
         <ReactQueryProvider>
@@ -47,8 +54,7 @@ export default function RootLayout({
           </ThemeProvider>
         </ReactQueryProvider>
         <Toaster />
-        <AdScript />
       </body>
     </html>
-  )
+  );
 }
